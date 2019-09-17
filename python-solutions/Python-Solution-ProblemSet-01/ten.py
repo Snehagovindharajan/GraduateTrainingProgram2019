f7822ae675b2db2eea0dc13646f0e14153a4dbee
Snehagovindharajan <36558121+Snehagovindharajan@users.noreply.github.com>
2019-09-17 08:34:14 +0530

    Update ten.py

--- python-solutions/Python-Solution-ProblemSet-01/ten.py
+++ python-solutions/Python-Solution-ProblemSet-01/ten.py
@@ -6,20 +6,19 @@
 # positive numbers? (Hint: think about changing low to ensure that the answer
 # lies within the region being searched.)
 
-x = -25
+x = 25
 epsilon = 0.01
 numGuesses = 0
 low = 0.0
-high = max(1.0, x)  # 25
-ans = (high + low) / 2.0  # 17.5 # 0.5
+high = max(1.0, x)
+ans = (high + low) / 2.0
 while abs(ans ** 2 - x) >= epsilon:
-    print('low =', low, 'high =', high, 'ans =', ans)  # 0.0 , 25 , 12.5
-
-numGuesses += 1  # 1
-if ans ** 2 < x:  # false
-    low = ans
-else:
-    high = ans  # high = 17.5
-    ans = (high + low) / 2.0  # 17.5
+    print('low =', low, 'high =', high, 'ans =', ans)
+    numGuesses += 1
+    if ans ** 2 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low) / 2.0
 print('numGuesses =', numGuesses)
 print(ans, 'is close to square root of', x)
